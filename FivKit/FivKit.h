e087cd1d34d0814c4372035effc3469e43975865
wuv1982 <wuv1982@gmail.com>
2019-01-31 10:55:06 +0900

    first commit

---
+++ FivKit/FivKit.h
@@ -0,0 +1,19 @@
+//
+//  FivKit.h
+//  FivKit
+//
+//  Created by wuwei on 2019/01/31.
+//  Copyright © 2019年 5v. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FivKit.
+FOUNDATION_EXPORT double FivKitVersionNumber;
+
+//! Project version string for FivKit.
+FOUNDATION_EXPORT const unsigned char FivKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FivKit/PublicHeader.h>
+
+
